25bfa07b835d7ad843a80e4f7f8fae0465064a29
Yasiru Lakshan <yasiru1999@gmail.com>
2022-10-20 15:36:12 +0530

    find manhatton function created

--- main.py
+++ main.py
@@ -20,8 +20,27 @@ def findMisplaced(start, final):
     return h
 
 
+def findIndex(arr, element):
+    size = len(arr)
+    for i in range(size):
+        for j in range(size):
+            if arr[i][j] == element:
+                return [i, j]
+
+
+def findManhattan(start, final):
+    h = 0
+    size = len(start)
+    for i in range(size):
+        for j in range(size):
+            if start[i][j] != "-" and start[i][j] != final[i][j]:
+                finalIndex = findIndex(final, start[i][j])
+                h += (abs(i - finalIndex[0]) + abs(j - finalIndex[1]))
+    return h
+
+
 for fileName in range(1):
     startMatrix = fileToMatrix(open("./start/" + str(fileName) + ".txt", "r"))
     endMatrix = fileToMatrix(open("./end/" + str(fileName) + ".txt", "r"))
 
-    print(findMisplaced(startMatrix, endMatrix))
+    print(findManhattan(startMatrix, endMatrix))
