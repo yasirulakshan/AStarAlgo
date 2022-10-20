fe4bc61716c047f310be5aef0f001b399caf7e39
Yasiru Lakshan <yasiru1999@gmail.com>
2022-10-20 15:30:25 +0530

    find misplaced tile function created

--- main.py
+++ main.py
@@ -11,8 +11,17 @@ def fileToMatrix(file):
     return arr
 
 
+def findMisplaced(start, final):
+    h = 0
+    for i in range(len(start)):
+        for j in range(len(start)):
+            if start[i][j] != "-" and start[i][j] != final[i][j]:
+                h += 1
+    return h
+
+
 for fileName in range(1):
     startMatrix = fileToMatrix(open("./start/" + str(fileName) + ".txt", "r"))
     endMatrix = fileToMatrix(open("./end/" + str(fileName) + ".txt", "r"))
-    print(startMatrix)
-    print(endMatrix)
+
+    print(findMisplaced(startMatrix, endMatrix))
