0d544c77b83cbbe425ee58c51e9eb669ec223cd5
Yasiru Lakshan <yasiru1999@gmail.com>
2022-10-20 15:27:09 +0530

    file read function created

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import copy
+
 from matrix import Matrix
 
-matrix1 = Matrix('a' , 'b' , 'c')
+
+def fileToMatrix(file):
+    arr = []
+    lines = file.read().split("\n")
+    for line in lines:
+        arr.append(line.split())
+    return arr
+
+
+for fileName in range(1):
+    startMatrix = fileToMatrix(open("./start/" + str(fileName) + ".txt", "r"))
+    endMatrix = fileToMatrix(open("./end/" + str(fileName) + ".txt", "r"))
+    print(startMatrix)
+    print(endMatrix)
--- matrix.py
+++ matrix.py
@@ -1,6 +1,9 @@
 class Matrix:
-    def __init__(self, values, g, h):
+    def __init__(self, values, g, h, lastMoved=None):
+        if lastMoved is None:
+            lastMoved = []
         self.values = values
         self.g = g
         self.h = h
         self.f = g + h
+        self.lastMoved = lastMoved
