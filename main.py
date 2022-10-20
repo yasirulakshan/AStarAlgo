eff7f5d8f6ee802e673306c4ed168d092decdf40
Yasiru Lakshan <yasiru1999@gmail.com>
2022-10-20 15:47:22 +0530

    find Dash function created

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ def fileToMatrix(file):
     return arr
 
 
-def findMisplaced(start, final):
+def findMisplaced(arr, final):
     h = 0
-    for i in range(len(start)):
-        for j in range(len(start)):
-            if start[i][j] != "-" and start[i][j] != final[i][j]:
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if arr[i][j] != "-" and arr[i][j] != final[i][j]:
                 h += 1
     return h
 
@@ -28,19 +28,35 @@ def findIndex(arr, element):
                 return [i, j]
 
 
-def findManhattan(start, final):
+def findManhattan(arr, final):
     h = 0
-    size = len(start)
+    size = len(arr)
     for i in range(size):
         for j in range(size):
-            if start[i][j] != "-" and start[i][j] != final[i][j]:
-                finalIndex = findIndex(final, start[i][j])
+            if arr[i][j] != "-" and arr[i][j] != final[i][j]:
+                finalIndex = findIndex(final, arr[i][j])
                 h += (abs(i - finalIndex[0]) + abs(j - finalIndex[1]))
     return h
+
+
+def findDash(arr):
+    length = len(arr)
+    dashed = []
+    for i in range(length):
+        for j in range(length):
+            if arr[i][j] == "-":
+                dashed.append([i, j])
+    return dashed
 
 
 for fileName in range(1):
     startMatrix = fileToMatrix(open("./start/" + str(fileName) + ".txt", "r"))
     endMatrix = fileToMatrix(open("./end/" + str(fileName) + ".txt", "r"))
 
-    print(findManhattan(startMatrix, endMatrix))
+    h = findManhattan(startMatrix, endMatrix)
+    # h = findMisplaced(startMatrix,endMatrix)
+
+    stack = [Matrix(startMatrix, 0, h)]
+    finalized = []
+
+    print(findDash(stack[0].values))
